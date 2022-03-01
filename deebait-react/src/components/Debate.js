b33baa090e5db6c201840337365ea34ed0f57526
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2022-03-01 14:12:11 +0800

    Add Debate UI Design

--- deebait-react/src/components/Debate.js
+++ deebait-react/src/components/Debate.js
@@ -1,8 +1,10 @@
 import { Component } from 'react';
 import { io } from 'socket.io-client';
 
+import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 import SendButton from './SendButton.js';
 
@@ -52,8 +54,30 @@ class Debate extends Component {
     render() {
         return (
             <Box>
-                <Box sx={{height: '70vh'}}>
+                <Box sx={{height: '70vh', overflowY: 'auto', mb: '10px' }}>
+                    <Box sx={{ width: '100%'}} mb={1}>
+                        <Alert severity='error'>This person prefers 'Cats' over 'Dogs'</Alert>
+                    </Box>
 
+                    <Box sx={{ width: '100%'}} mb={1}>
+                        <Alert severity='error'>This person is a 'Top' not a 'Bottom'</Alert>
+                    </Box>
+
+                    <Box sx={{ width: '100%' }} mb={1}>
+                        <Box p={1} mt={1} borderRadius={2} sx={{ clear: 'both', float: 'left', backgroundColor: '#ddd', display: 'inline-block'}}>
+                            <Typography variant="body1">
+                                Bruh, dogs suck.
+                            </Typography>
+                        </Box>
+                    </Box>
+
+                    <Box sx={{ width: '100%' }} mb={1}>
+                        <Box p={1} mt={1} borderRadius={2} sx={{ clear: 'both', float: 'right', backgroundColor: '#ff1744', color: 'white', display: 'inline-block'}}>
+                            <Typography variant="body1">
+                                Says the pussy on top.
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Box>
                 <TextField 
                     style={{borderRadius: '100px'}}
